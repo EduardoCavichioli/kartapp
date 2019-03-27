2e48ed3926167fcb8fc052207ca41a495127ff24
Eduardo Cavichioli <eduardocavichioli@Eduardos-MacBook-Pro.local>
2019-03-27 15:28:50 -0300

    simple router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        My App
-      </div>
+      <Router>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Switch>
+      </Router>
     );
   }
 }
