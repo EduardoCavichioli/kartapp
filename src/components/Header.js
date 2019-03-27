45fa4f27951242923fb7cf3a763fd66d9a939c1f
Eduardo Cavichioli <eduardocavichioli@Eduardos-MacBook-Pro.local>
2019-03-27 16:55:00 -0300

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import { Menu, Container } from 'semantic-ui-react';
+import { Segment, Menu, Container } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <Menu inverted style={{ marginBottom: 0 }} >
+      <Segment vertical inverted>
+      <Menu style={{ marginBottom: 0 }} inverted>
         <Container>
           <Menu.Item as={Link} to='/'>Home</Menu.Item>
           <Menu.Item as={Link} to='/about'>About</Menu.Item>
         </Container>
       </Menu>
+      </Segment>
     );
   }
 }
