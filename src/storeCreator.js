ee1229eb26cc84c5b0f10b1d775354b69ddeb08b
Eduardo Cavichioli <eduardocavichioli@Eduardos-MacBook-Pro.local>
2019-03-31 11:11:04 -0300

    adding redux logger

--- src/storeCreator.js
+++ src/storeCreator.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import logger from 'redux-logger';
 
 export const store = createStore(
   reducers,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, logger)
 );
